877afd687e172199963d59ee3d8fbf4b12086f2b
nzr-xdev <nzr.dev@proton.me>
2026-05-13 20:55:13 +0300

    some fixes and cosmetics | missing data - fixed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from tkinter import filedialog, messagebox
 from core import DataValidator
+from time import perf_counter
 import json
 import os
 
@@ -16,6 +17,8 @@ class DataValidatorGUI(ctk.CTk):
         self.path_data = None
         self.path_config = None
         self.ignore_missing_var = ctk.BooleanVar(value=False)
+        try: self.iconbitmap("assets/icon.ico")
+        except Exception as e: print(f'Failed to load the icon \n{e}')
 
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
@@ -23,9 +26,10 @@ class DataValidatorGUI(ctk.CTk):
 
         self.result_box = ctk.CTkTextbox(self, font=("Consolas", 13), fg_color="#121212")
         self.result_box.grid(row=0, column=0, sticky="nsew", padx=20, pady=20)
-        self.result_box.tag_config("error_line", background="#4b1b1b")
-        self.result_box.tag_config("ok_line", background="#1b3b1b")
-        self.result_box.tag_config("error_detail", foreground="#e74c3c")
+        self.result_box.tag_config("error_line", background="#4e1a1a")
+        self.result_box.tag_config("ok_line", background="#2d5f2d")
+        self.result_box.tag_config("error_detail", foreground="#e4503f")
+        self.result_box.tag_config("missing_line", background="#96962f", foreground="white")
         
         self.bottom_panel = ctk.CTkFrame(self, height=50, corner_radius=10)
         self.bottom_panel.grid(row=1, column=0, sticky="ew", padx=5, pady=(0,10))
@@ -80,14 +84,19 @@ class DataValidatorGUI(ctk.CTk):
         path = filedialog.askopenfilename(filetypes=[("JSON files", "*.json")])
         if path:
             self.path_config = path
-            self.conf_label.configure(text=f"Config: {os.path.basename(path)}", text_color="#f1c40f")
+            self.conf_label.configure(text=f"Config: {os.path.basename(path)}", text_color="#d4af18")
 
     def _handle_run(self):
             if not self.path_data or not self.path_config:
                 messagebox.showwarning("Warn", "Choose the data/config file!")
                 return
             
-            raw_input = self.result_box.get("1.0", "end-1c").strip()
+            try:
+                with open(self.path_data, 'r', encoding='utf-8') as f:
+                    raw_input = f.read().strip()
+            except Exception:
+                raw_input = self.result_box.get("1.0", "end-1c").strip()
+
             if not raw_input: 
                 messagebox.showwarning("Warn", "Here is no data")
                 return
@@ -95,10 +104,13 @@ class DataValidatorGUI(ctk.CTk):
             self.result_box.configure(state="normal")
             self.result_box.delete("1.0", "end")
 
+            t_start = perf_counter()
+
             try:
                 parsed_data = json.loads(raw_input)
                 formatted_json = json.dumps(parsed_data, indent=4, ensure_ascii=False)
                 lines = formatted_json.split('\n')
+                ignore_missing = self.ignore_missing_var.get()
                 
                 with open(self.path_config, 'r', encoding='utf-8') as f:
                     self.validator._generate_model(json.load(f))
@@ -108,6 +120,7 @@ class DataValidatorGUI(ctk.CTk):
                 
                 c_ok, c_err = 0, 0
                 current_case_idx = -1
+                used_errors = []
 
                 for i, line_content in enumerate(lines):
                     line_num = i + 1
@@ -115,15 +128,20 @@ class DataValidatorGUI(ctk.CTk):
                     
                     if "{" in clean_line:
                         current_case_idx += 1
+                        used_errors = []
                     
                     line_errors = []
+                    is_missing_type = False
+                    
                     if 0 <= current_case_idx < len(report):
                         case_res = report[current_case_idx]
+                        
                         if case_res['status'] == 'error':
                             for detail in case_res.get('details', []):
                                 field = detail.split(':')[0]
                                 if f'"{field}"' in line_content:
                                     line_errors.append(detail)
+                                    used_errors.append(detail)
 
                     prefix = f"{line_num} "
                     if line_errors:
@@ -137,13 +155,26 @@ class DataValidatorGUI(ctk.CTk):
                     else:
                         self.result_box.insert("end", f"{prefix}  {line_content}\n")
 
+                    if "}" in clean_line and 0 <= current_case_idx < len(report):
+                        case_res = report[current_case_idx]
+                        if case_res['status'] == 'error':
+                            missing_errors = [d for d in case_res['details'] if d not in used_errors]
+                            
+                            if missing_errors and not ignore_missing:
+                                for m_err in missing_errors:
+                                    self.result_box.insert("end", f"      ⚠ MISSING FIELD: {m_err}\n", "missing_line")
+                                    c_err += 1
+
                 self._update_stats(c_ok, c_err)
+                self.result_box.insert("end", f"Time elapsed: {perf_counter() - t_start:.2f} sec", "ok_line")
 
             except json.JSONDecodeError as je:
                 self.result_box.insert("end", f"❌ JSON file has an error:\n{str(je)}", "error_line")
                 messagebox.showerror("Format Error", f"Failed to parse JSON file. Check terminal output", "error_line")
+                self.result_box.insert("end", f"Time elapsed: {perf_counter() - t_start:.2f} sec", "ok_line")
             except Exception as e:
                 self.result_box.insert("end", f"❌ SYSTEM ERROR: {e}", "error_line")
+                self.result_box.insert("end", f"Time elapsed: {perf_counter() - t_start:.2f} sec", "ok_line")
             
             self.result_box.configure(state="disabled")
 
